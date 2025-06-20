598e6d83f0e59bc5c026b427d9c020eaff025448
The Shadow Ball <59938664+red-rc@users.noreply.github.com>
2025-06-20 14:48:58 +0300

    Add project files.

---
+++ PixelArtEditor/ViewModels/MainViewModel.cs
@@ -0,0 +1,7 @@
+﻿
+namespace PixelArtEditor.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ PixelArtEditor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PixelArtEditor.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
