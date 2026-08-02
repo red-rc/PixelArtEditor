babffdd6a695c72341fbcf34a339f82d9bd1e05d
The Shadow Ball <59938664+red-rc@users.noreply.github.com>
2026-08-02 23:58:25 +0300

    restyled menu also

--- PixelArtEditor/Controls/MenuBar.axaml.cs
+++ PixelArtEditor/Controls/MenuBar.axaml.cs
@@ -1,8 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Threading;
-using System;
-
 namespace PixelArtEditor.Controls;
 
 public partial class MenuBar : UserControl
@@ -10,22 +6,5 @@ public partial class MenuBar : UserControl
     public MenuBar()
     {
         InitializeComponent();
-    }
-
-    private void OnMenuFlyoutOpened(object? sender, EventArgs e)
-    {
-        if (sender is Flyout { Target: Button btn })
-            btn.Classes.Add("MenuOpen");
-    }
-
-    private void OnMenuFlyoutClosed(object? sender, EventArgs e)
-    {
-        if (sender is Flyout { Target: Button btn })
-            btn.Classes.Remove("MenuOpen");
-    }
-
-    private void CloseFileFlyout(object? sender, RoutedEventArgs e)
-    {
-        Dispatcher.UIThread.Post(() => MenuItem.Flyout?.Hide(), DispatcherPriority.Background);
     }
 }
